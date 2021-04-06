b6aeab591399cfdad668c9d6d363995b453018e6
wonjun <icecube9670@gmail.com>
2021-04-06 10:48:05 +0900

    Initial Commit

---
+++ duru/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  duru
+//
+//  Created by 양원준 on 2021/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
